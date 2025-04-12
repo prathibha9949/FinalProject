a9634497183cf9e11bf05ac6643a21b182e169ba
Sandela Suhitha <suhitha@Suhitha.local>
2025-04-12 22:56:05 +0530

    Updated backend URL to deployed backend

--- ohmnamahshivaya/FARMERFINAL107/FARMER107/Backend/server.js
+++ ohmnamahshivaya/FARMERFINAL107/FARMER107/Backend/server.js
@@ -50,10 +50,7 @@ if (!fs.existsSync(uploadDir)) {
 
 // MongoDB Connection
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB Connected"))
   .catch((err) => console.error("❌ MongoDB Connection Error:", err));
 
