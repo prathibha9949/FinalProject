48cd08dd299407fef2769121fa24ee62b6dd17f5
Sandela Suhitha <suhitha@Suhitha.local>
2025-04-13 01:12:35 +0530

    Updated backend URL to deployed backend

--- ohmnamahshivaya/FARMERFINAL107/FARMER107/Frontend/src/components/Navbar.js
+++ ohmnamahshivaya/FARMERFINAL107/FARMER107/Frontend/src/components/Navbar.js
@@ -5,7 +5,7 @@ import '../App.css';  // ✅ Fixed path
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg " style={{ height: "60px", position: "fixed", width: "100%", top: "0", zIndex: "1000","background-color": "#597D35",color: "white"  }}>
-      <div className="container-fluid text-white">
+      <div className="container-fluid text-black">
         <button
           className="navbar-toggler"
           type="button"
@@ -21,19 +21,19 @@ const Navbar = () => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link to="/" className="nav-link text-white">Home</Link>
+              <Link to="/" className="nav-link text-black">Home</Link>
             </li>
             <li className="nav-item">
-              <Link to="/about" className="nav-link text-white">About</Link>
+              <Link to="/about" className="nav-link text-black">About</Link>
             </li>
           </ul>
 
           <ul className="navbar-nav mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link to="/login" className="nav-link text-white">Login</Link>
+              <Link to="/login" className="nav-link text-black">Login</Link>
             </li>
             <li className="nav-item">
-              <Link to="/signup" className="nav-link text-white">Signup</Link>
+              <Link to="/signup" className="nav-link text-black">Signup</Link>
             </li>
             
             
